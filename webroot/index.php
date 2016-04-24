1eb0df9d429f0ee08c7952e388b7e81217de682d
andrejasd <andrej_asd@bigmir.net>
2016-04-24 11:23:47 +0300

    lesson 12

--- webroot/index.php
+++ webroot/index.php
@@ -10,6 +10,7 @@ define('ROOT', dirname(dirname(__FILE__)));
 define('VIEWS_PATH', ROOT.DS.'views');
 
 require_once (ROOT.DS.'lib'.DS.'init.php');
+
 /*
 $router = new Router($_SERVER['REQUEST_URI']);
 
@@ -21,5 +22,8 @@ print_r('Action to be called: '.$router->getMethodPrefix().$router->getAction().
 echo 'Params: ';
 print_r($router->getParams());
 */
+
+// Session::setFlash('Test flash message');
+
 App::run($_SERVER['REQUEST_URI']);
 
